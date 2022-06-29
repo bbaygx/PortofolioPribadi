c3c8d6bf462db2a0810febeeb14e31e4c88736e3
BayuYT <bayuputraefendi993@gmail.com>
2022-06-29 20:32:11 +0700

    Ada Beberapa Update & Perubahan

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function() {
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer", "Cyber Security", "Programmer"],
+        strings: ["Web Developer", "CyberSecurity", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer", "Cyber Security", "Programmer"],
+        strings: ["Web Developer", "CyberSecurity", "Programmer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
